f43cab32597d4011afcabd8445ed1f5fd3cf8798
Yakup Düzgün <yakup.ydzn71@gmail.com>
2018-04-05 10:29:20 +0300

    Update aosp_capricorn.mk

--- aosp_capricorn.mk
+++ aosp_capricorn.mk
@@ -1,9 +1,37 @@
-$(call inherit-product, device/xiaomi/capricorn/full_capricorn.mk)
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from capricorn device
+$(call inherit-product, device/xiaomi/capricorn/device.mk)
+
+# Inherit some common PE stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# GApps
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_ARCORE := true
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_capricorn
 PRODUCT_DEVICE := capricorn
 PRODUCT_BRAND := Xiaomi
