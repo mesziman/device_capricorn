dd2f0cbddbc98432e447c563376021809cae0526
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2018-06-14 08:58:57 +0000

    remove los ovrlay

--- du.mk
+++ du.mk
@@ -39,5 +39,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys"
-
+TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
